c0bfc3c2d404331d4d3a1dafe49a76a93dd82404
Arsen Gasparyan <sir.frootloops@gmail.com>
2015-05-11 12:19:56 +0300

    Initial commit

---
+++ ReWheely/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReWheely
+//
+//  Created by Arsen Gasparyan on 11/05/15.
+//  Copyright (c) 2015 Arsen Gasparyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
