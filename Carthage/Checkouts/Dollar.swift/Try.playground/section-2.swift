6b78738a52648d2295da48004ede7613091743a4
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2015-07-11 22:32:36 +0300

    Remove unused libs

--- Carthage/Checkouts/Dollar.swift/Try.playground/section-2.swift
+++
@@ -1,5 +0,0 @@
-import Dollar
-import Cent
-
-
-
--- Carthage/Checkouts/Kingfisher/Pods/Nocilla/Nocilla/Hooks/ASIHTTPRequest/ASIHTTPRequestStub.h
+++
@@ -1,6 +0,0 @@
-@interface ASIHTTPRequestStub : NSObject
-- (NSInteger)stub_responseStatusCode;
-- (NSData *)stub_responseData;
-- (NSDictionary *)stub_responseHeaders;
-- (void)stub_startRequest;
-@end
